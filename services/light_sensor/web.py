3e572e27d9ee68541ce5bf9deab1353fd9ca4c23
rafael.romanotero-at-570034927297 <rafael.romanotero@teejlab.com>
2021-08-16 23:06:27 -0700

    update

--- services/light_sensor/web.py
+++ services/light_sensor/web.py
@@ -9,7 +9,7 @@ app.logger.setLevel(logging.INFO)
 @app.route("/")
 def hello_world():
     app.logger.info("default path (light sensor)")
-    return "Hello, World!!"
+    return "Hello, World!"
 
 
 @app.route("/hello/<username>")
