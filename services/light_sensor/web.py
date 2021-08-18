4ba239cd8f2d6b9eaf54638d1225618fadac57d2
rafael.romanotero-at-570034927297 <rafael.romanotero@teejlab.com>
2021-08-17 23:43:56 -0700

    bump up to 5

--- services/light_sensor/web.py
+++ services/light_sensor/web.py
@@ -8,8 +8,8 @@ app.logger.setLevel(logging.INFO)
 
 @app.route("/")
 def hello_world():
-    app.logger.info("default path (light sensor) 4")
-    return "Hello, World 4!"
+    app.logger.info("default path (light sensor) 5")
+    return "Hello, World 5!"
 
 
 @app.route("/hello/<username>")
