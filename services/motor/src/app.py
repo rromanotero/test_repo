4a382a262139968806787d321d41ce3ab7e1aec6
rafael.romanotero-at-570034927297 <rafael.romanotero@teejlab.com>
2021-09-11 01:56:17 -0700

    get it back to how it was

--- services/motor/src/app.py
+++ services/motor/src/app.py
@@ -1,19 +1,14 @@
-import logging
-from flask import Flask
-from markupsafe import escape
+from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
-app.logger.setLevel(logging.INFO)
-
 
 @app.route("/")
-def hello_world():
-    app.logger.info("default path (motor) 7")
-    return "Hello, World 7!"
+def salute():
+    return jsonify({
+        "server_status" : 'OKKKAAAAAYYY (motor)',
+        "ENV" : os.environ['ENV']
+        })
 
-
-@app.route("/hello/<username>")
-def hello_user(username):
-    # say hello to that user
-    app.logger.info("/hello path. User: " + username)
-    return "Hello %s" % escape(username)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
